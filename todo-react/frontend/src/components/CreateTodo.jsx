c5943bf20ba69f2aeb7080b12772d783cc69fa40
samarsrivastav <samarsrivastav69@gmail.com>
2024-09-21 20:02:09 +0530

    frontend connection done

--- todo-react/frontend/src/components/CreateTodo.jsx
+++ todo-react/frontend/src/components/CreateTodo.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react"
+
+const addfunction = (title, desc) => {
+    fetch("http://localhost:3000/todo", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        description: desc,
+        completed: false,
+      }),
+      headers: {
+        "Content-Type": "application/json", 
+      },
+    })
+      .then(async function (res) {
+        const json = await res.json();
+        console.log(json);
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+      });
+  };
 function CreateTodo(){
+    const [title,setTitle]=useState("")
+    const [description,setDescription]=useState("")
     return(
         <>
         <div>
-            <input type="text" placeholder="title" />
+            <input type="text" placeholder="title" id="title" onChange={function(e){
+                const value=e.target.value
+                setTitle(value)
+            }}/>
             <br/>
-            <input type="text" placeholder="title" />
+            <input type="text" placeholder="description" id="desc" onChange={function(e){
+                const value=e.target.value
+                setDescription(value)
+            }}/>
             <br/>
 
-            <button>Add todo</button>
+            <button onClick={() => addfunction(title, description)}>Add todo</button>
         </div>
         </>
     )
--- todo-react/frontend/src/components/Todos.jsx
+++ todo-react/frontend/src/components/Todos.jsx
@@ -1,3 +1,22 @@
+
+const markAsCompleted=(id,title,desc)=>{
+    console.log(id)
+    fetch("http://localhost:3000/completed",{
+        method:"PUT",
+        body: JSON.stringify({
+            id:id,
+            title:title,
+            description:desc
+        }),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+    .then(async function(res){
+        const json=await res.json()
+        console.log(json)
+    })
+}
 function Todos({todos}){
     return (
     <>
@@ -7,7 +26,7 @@ function Todos({todos}){
                 <div>
                     <h1>{item.title}</h1>
                     <h2>{item.description}</h2>
-                    <button id={item._id}>{item.completed==true?"completed":"Mark as completed"}</button>
+                    <button id={item._id} onClick={()=>markAsCompleted(item._id,item.title,item.description)}>{item.completed==true?"completed":"Mark as completed"}</button>
                 </div>
             )
         })}
