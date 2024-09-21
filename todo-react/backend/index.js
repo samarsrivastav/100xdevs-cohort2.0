b6a65db809dc677931d79be88da27ca15d7a0932
samarsrivastav <samarsrivastav69@gmail.com>
2024-09-21 13:46:42 +0530

    backend done

--- todo-react/backend/index.js
+++ todo-react/backend/index.js
@@ -1,9 +1,9 @@
 const express=require("express")
 const app=express()
 const {createTodo}=require("./types.js")
+const {todo} = require("./db.js")
 app.use(express.json())
-
-app.post("/todo",function(req,res){
+app.post("/todo",async function(req,res){
     const createPayLoad=req.body
     const parsePayLoad=createTodo.safeParse(createPayLoad);
     if(!parsePayLoad.success){
@@ -12,13 +12,23 @@ app.post("/todo",function(req,res){
         })
         return
     }
-    //put in mongodb
+    const todoAdded= await todo.create({
+        title:createPayLoad.title,
+        description:createPayLoad.description,
+        completed:false
+    })
+    res.json({
+        msg:"todo added",
+        id:todoAdded._id
+    })
 })
-app.get("/todos",function(req,res){
-    
-
+app.get("/todos",async function(req,res){
+    const todos=await todo.find({});
+    res.send({
+        todos:todos
+    })
 })
-app.put("/completed",function(req,res){
+app.put("/completed",async function(req,res){
     const createPayLoad=req.body
     const parsePayLoad=createTodo.safeParse(createPayLoad);
     if(!parsePayLoad.success){
@@ -26,7 +36,14 @@ app.put("/completed",function(req,res){
             msg:"send wrong input"
         })
         return
-    }else{
-
     }
+    const todoUpdated=await todo.updateOne({
+        _id:req.body.id
+    },{
+        completed:true
+    })
+    res.send({
+        msg:"Todo Marked as completed"
+    })
 })
+app.listen(3000)
