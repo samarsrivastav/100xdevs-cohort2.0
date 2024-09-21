623226ffcd357e07d51cbff3159432dff42356ad
samarsrivastav <samarsrivastav69@gmail.com>
2024-09-21 20:53:04 +0530

    useEffect added

--- todo-react/frontend/src/App.jsx
+++ todo-react/frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import CreateTodo from './components/CreateTodo'
 import Todos  from './components/Todos'
 
 function App() {
   const [todos,setTodo]=useState([])
-  fetch("http://localhost:3000/todos")
-  .then(async function (res) {
-    const json = await res.json()
-    setTodo(json.todos)
-  })
+  useEffect(()=>{
+    fetch("http://localhost:3000/todos")
+    .then(async function (res) {
+      const json = await res.json()
+      setTodo(json.todos)
+    })
+  },[todos])
+  
   return (
     <>
       <CreateTodo/>
