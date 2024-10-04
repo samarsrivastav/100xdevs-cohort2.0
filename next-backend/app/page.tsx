cac752ba74c532141e4f6e3b3f17da99a45fd4b8
samarsrivastav <samarsrivastav69@gmail.com>
2024-10-05 01:03:03 +0530

    changing the call style

--- next-backend/app/page.tsx
+++ next-backend/app/page.tsx
@@ -1,27 +1,32 @@
-import axios from "axios";
+import client from '@/db'
+//instead of sending a request from a server back to the server-> directly call the database
+
 
 async function getUserDetails() {
-  const response = await axios.get("http://localhost:3000/api/user")
-	return response.data;
+  const res=await client.user.findMany()
+  return res
 }
 
 export default async function Home() {
   const userData = await getUserDetails();
   return (
     <>
-    {(userData.res) && (userData.res).length > 0 ? (
-        (userData.res).map((item: any) => (
-          <div className="flex flex-col " key={item.id}>
-            <div className="flex justify-center">
-              <div className="border p-8 rounded">
-                <div>Name: {item?.username}</div>
+    <div className=" m-3 ">
+      {(userData) && (userData).length > 0 ? (
+          (userData).map((item: any) => (
+            <div className="flex flex-col " key={item.id}>
+              <div className="">
+                <div className="border p-8 rounded">
+                  <div>id: {item?.id}</div>
+                  <div>Name: {item?.username}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))
-      ) : (
-        <div>No users found</div>
-      )}
+          ))
+        ) : (
+          <div>No users found</div>
+        )}
+    </div>
     </>
   );
 }
--- next-backend/components/Signup.tsx
+++ next-backend/components/Signup.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
-
+import {signup} from "@/app/actions/user"
 export function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -25,11 +25,12 @@ export function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
                         <button onClick={async () => {
-                            const response = await axios.post("http://localhost:3000/api/user", {
-                                username,
-                                password
-                            });
-                            router.push("/")
+                            try{
+                               const res=JSON.stringify( await signup(username,password));
+                                router.push("/")
+                            }catch{
+                                console.log("\n\nerror---------")
+                            }
                         }} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
                     </div>
                 </div>
